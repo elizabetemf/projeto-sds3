56cbaa05aa44117985cc9b263d721ce44db841fc
Elizabete Fujiwara <elizabetemf@gmail.com>
2021-05-08 11:39:12 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsvendas.service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+
+	public List<SellerDTO> findAll() {
+		List<Seller> result =  repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList()); // pega o resultado da lista que é do tipo seller, converte para s
+																						//stream.map, e cada elemento do map, que é o x, vai ser convertido em
+																						// um sellerdto, ai o  resultado tem que ser convertido de volta numa list.
+	}
+}
