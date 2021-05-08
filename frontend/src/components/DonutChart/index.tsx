47be7b46a5074619ca9eb2651c36e3f16a3f7194
Elizabete Fujiwara <elizabetemf@gmail.com>
2021-05-08 18:45:08 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,40 +1,47 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
-type ChartData  = {
-    labels : string[];
-    series : number[]
+type ChartData = {
+    labels: string[];
+    series: number[]
 }
 
 const DonutChart = () => {
-   
+
     //FORMA ERRADA, declarar a variavel desse jeito: let chartData, no proximo passo vai usar react-roots
-    let chartData : ChartData = { labels: [], series: []};
+    //let chartData : ChartData = { labels: [], series: []};
+
+    //forma correta de inicializar usando o useState, o setChartDate é uma função que atualiza o valor da variavel a esquerda que é o chartData 
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     //FORMA ERRADA ainda
-//    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-//    .then( (response) => {
-//        console.log(response.data);
-//    })
+    //    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //    .then( (response) => {
+    //        console.log(response.data);
+    //    })
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then( (response) => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map( x => x.sellerName);
-            const mySum = data.map( x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySum = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySum};
+                // chartData = { labels: myLabels, series: mySum};
+                setChartData({ labels: myLabels, series: mySum }); // agora ao inves de ter a lista, a funcao setChartData vai receber os objetos que foi montado com os dados que foi recebido do back
 
-            //console.log(response.data);
-            console.log(chartData);
-        })
-//    const mockData = {
-//        series: [477138, 499928, 444867, 220426, 473088],
-//        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-//    }
+                //console.log(response.data);
+               // console.log(chartData);
+            });
+    }, []);
+
+    //   const mockData = {
+    //        series: [477138, 499928, 444867, 220426, 473088],
+    //        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //    }
 
     const options = {
         legend: {
